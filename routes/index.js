a5856cc67e9a7bf82bd13f945971b79da47ace48
RoenissMoon <roeniss2@gmail.com>
2020-02-17 01:34:21 +0900

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const { uuid } = require("uuidv4");
+const UserLogic = require("../logic/user");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+router.get("/login", (req, res, next) => res.render("login"));
+router.post("/login", UserLogic.login);
+
+router.use("*", UserLogic.authenticate);
+router.get("/clients", (req, res, next) => {
+  const { user } = res.locals;
+  res.render("clients", { user });
 });
+router.get("/", (req, res, next) => res.render("dashboard"));
 
 module.exports = router;
