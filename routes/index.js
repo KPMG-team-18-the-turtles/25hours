bbfa8f1a023807aae5bea06f3d54b87e71daa2e8
RoenissMoon <roeniss2@gmail.com>
2020-02-21 01:51:41 +0900

    finish dashboard html and other pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const { uuid } = require("uuidv4");
 const UserLogic = require("../logic/user");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
 
 const router = express.Router();
 
@@ -13,5 +17,22 @@ router.get("/clients", (req, res, next) => {
   res.render("clients", { user });
 });
 router.get("/", (req, res, next) => res.render("dashboard"));
+
+router.post("/record", upload.single("audio"), (req, res, next) => {
+  if (!!req.file) {
+    const { filename } = req.file;
+
+    const targetFile = path.join(__dirname, "..", "uploads", filename);
+    const resultWavFile = path.join(__dirname, "..", "uploads", "record.wav");
+
+    ffmpeg(targetFile)
+      .toFormat("wav")
+      .save(resultWavFile); //path where you want to save your file
+
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
